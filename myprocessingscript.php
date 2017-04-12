64eef4a5adfd4cef8ced80e9ceca53c2a438a58d
itsCozmikk <itscozmikk@gmail.com>
2017-04-12 01:14:29 -0400

    Update myprocessingscript.php

--- myprocessingscript.php
+++ myprocessingscript.php
@@ -1,14 +1,9 @@
 <?php
-if(isset($_POST['field1']) && isset($_POST['field2'])) {
-    $data = $_POST['field1'] . '-' . $_POST['field2'] . "\n";
-    $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$ret bytes written to file";
-    }
+$txt = "data.txt"; 
+if (isset($_POST['field1']) && isset($_POST['field2'])) { // check if both fields are set
+    $fh = fopen($txt, 'a'); 
+    $txt=$_POST['field1'].' - '.$_POST['field2']; 
+    fwrite($fh,$txt); // Write information to the file
+    fclose($fh); // Close the file
 }
-else {
-   die('no post data to process');
-}
+?>
